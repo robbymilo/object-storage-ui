5cf766240039896ad22f18a31b3fb9a94407f6e1
robbymilo <robbymilo@fastmail.com>
2022-12-02 22:46:53 +0100

    download working

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
-  "strings"
+	"strings"
+	"time"
+	"strconv"
 
 	"cloud.google.com/go/storage"
 	"google.golang.org/api/iterator"
@@ -31,16 +35,56 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-  if (strings.HasSuffix(r.URL.Path, "/") == true) {
-    listDir(w, r)
-  } else {
-    serveFile(w, r)
-  }
+	if strings.HasSuffix(r.URL.Path, "/") == true {
+		listDir(w, r)
+	} else {
+		serveFile(w, r)
+	}
 
 }
 
 func serveFile(w http.ResponseWriter, r *http.Request) {
-  fmt.Println(r.URL.Path)
+	fmt.Println(r.URL.Path)
+
+	start := time.Now()
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	// Sets the name for the new bucket.
+	bucketName := "staging-static-grafana-com"
+
+	// Creates a Bucket instance.
+	bucket := client.Bucket(bucketName)
+	oh := bucket.Object(r.URL.Path[1:])
+	objAttrs, err := oh.Attrs(ctx)
+	if err != nil {
+		if os.Getenv("LOGGING") == "true" {
+			elapsed := time.Since(start)
+			log.Println("| 404 |", elapsed.String(), r.Host, r.Method, r.URL.Path)
+		}
+		http.Error(w, "Not found", 404)
+		return
+	}
+	o := oh.ReadCompressed(true)
+	rc, err := o.NewReader(ctx)
+	if err != nil {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	defer rc.Close()
+
+	w.Header().Set("Content-Type", objAttrs.ContentType)
+	w.Header().Set("Content-Encoding", objAttrs.ContentEncoding)
+	w.Header().Set("Content-Length", strconv.Itoa(int(objAttrs.Size)))
+	w.WriteHeader(200)
+	if _, err := io.Copy(w, rc); err != nil {
+		return
+	}
 }
 
 func listDir(w http.ResponseWriter, r *http.Request) {
@@ -58,15 +102,15 @@ func listDir(w http.ResponseWriter, r *http.Request) {
 	// Creates a Bucket instance.
 	bucket := client.Bucket(bucketName)
 
-  prefix := ""
+	prefix := ""
 	delim := "/"
 
-  if (r.URL.Path[1:] != "") {
-    // non-root path
-    prefix = r.URL.Path[1:]
-  }
+	if r.URL.Path[1:] != "" {
+		// non-root path
+		prefix = r.URL.Path[1:]
+	}
 
-  fmt.Println(prefix, delim)
+	fmt.Println(prefix, delim)
 
 	it := bucket.Objects(ctx, &storage.Query{
 		Prefix:    prefix,
@@ -91,9 +135,8 @@ func listDir(w http.ResponseWriter, r *http.Request) {
 		// fmt.Println(attrs.Prefix, attrs.Name)
 	}
 
-  lp := filepath.Join("templates", "layout.html")
+	lp := filepath.Join("templates", "layout.html")
 	fp := filepath.Join("templates", "example.html")
-
 
 	tmpl, _ := template.ParseFiles(lp, fp)
 	varmap := map[string]interface{}{
